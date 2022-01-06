a18959a34601d731fb36c0a8fca16ea87c5951e0
Yarles404 <yarlescy@gmail.com>
2022-01-06 11:55:09 -0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IYarles.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
