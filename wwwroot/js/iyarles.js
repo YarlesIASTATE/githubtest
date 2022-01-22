d72571c976931d2883980128565efd75cc0e4d8d
Yarles404 <yarlescy@gmail.com>
2022-01-21 18:22:14 -0600

    wwwroot broken

--- wwwroot/js/iyarles.js
+++ wwwroot/js/iyarles.js
@@ -1,19 +1,21 @@
-const burger = document.querySelector('#burger');
-const nav = document.querySelector('#myNav');
-
-burger.addEventListener('click', () => {
-    nav.classList.toggle('is-active');
-    burger.classList.toggle('is-active');
-});
-
-const modal = document.querySelector('#climb');
-const modalToggle = document.querySelector('#climbToggle');
-const modalClose = document.querySelector('.modal-close');
-
-modalToggle.addEventListener('click', () => {
-    modal.classList.toggle('is-active');
-});
-
-modalClose.addEventListener('click', () => {
-    modal.classList.toggle('is-active');
+$(document).ready(function() {
+    const burger = document.querySelector('#burger');
+    const nav = document.querySelector('#myNav');
+    
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('is-active');
+        burger.classList.toggle('is-active');
+    });
+    
+    const modal = document.querySelector('#climb');
+    const modalToggle = document.querySelector('#climbToggle');
+    const modalClose = document.querySelector('.modal-close');
+    
+    modalToggle.addEventListener('click', () => {
+        modal.classList.toggle('is-active');
+    });
+    
+    modalClose.addEventListener('click', () => {
+        modal.classList.toggle('is-active');
+    });
 });
